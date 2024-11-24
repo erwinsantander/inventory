a4c315cd5c055019d15b46a644a73136bf528fe0
erwinsantander <santandererwin4@gmail.com>
2024-11-24 10:18:45 -0800

    commit

--- includes/config.php
+++ includes/config.php
@@ -13,8 +13,8 @@
 |
 */
 define( 'DB_HOST', '127.0.0.1' );          // Set database host
-define( 'DB_USER', 'root' );             // Set database user
-define( 'DB_PASS', '' );             // Set database password
-define( 'DB_NAME', 'inventory_system' );        // Set database name
+define( 'DB_USER', 'u510162695_ancminimart' );             // Set database user
+define( 'DB_PASS', '1Ancminimart' );             // Set database password
+define( 'DB_NAME', 'u510162695_ancminimart' );        // Set database name
 
 ?>
