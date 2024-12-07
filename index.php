a82d8701c11caf104dcbce41e5b43c252b896c55
erwinsantander <santandererwin4@gmail.com>
2024-12-07 14:35:14 -0800

    commit

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<?php
-    ob_start();
-    require_once('includes/load.php');
-    if ($session->isUserLoggedIn()) {
-        redirect('admin.php', false);
-    }
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
