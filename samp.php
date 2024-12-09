1aab84cdab35ae1ae6e0f82aceaeebd66c617a2e
erwinsantander <santandererwin4@gmail.com>
2024-12-09 01:52:06 -0800

    commit

--- samp.php
+++ samp.php
@@ -11,6 +11,12 @@ if (substr($request, -4) == '.php') {
     header("Location: $new_url", true, 301);
     exit();
 }
+
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Permissions-Policy: geolocation=(self), microphone=()");
 ?>
 
 <!DOCTYPE html>
