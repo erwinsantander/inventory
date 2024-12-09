49bbec799243c504dfabe0749ff8de53e4d625cc
erwinsantander <santandererwin4@gmail.com>
2024-12-09 00:54:42 -0800

    commit

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ if ($session->isUserLoggedIn()) {
     redirect('admin.php', false);
 }   
 
-
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
