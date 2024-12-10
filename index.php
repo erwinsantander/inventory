cb64093c5e1d582a37e19b1211b5ffcbec071b7c
erwinsantander <santandererwin4@gmail.com>
2024-12-09 20:52:05 -0800

    commit

--- index.php
+++ index.php
@@ -12,11 +12,7 @@ if (substr($request, -4) == '.php') {
     exit();
 }
 
-header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
-header("X-Frame-Options: SAMEORIGIN");
-header("X-Content-Type-Options: nosniff");
-header("Referrer-Policy: strict-origin-when-cross-origin");
-header("Permissions-Policy: geolocation=(self), microphone=()");
+
 ?>
 
 <!DOCTYPE html>
