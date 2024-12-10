8bbf2241b01fb7f0838fa11d6fe81eda848e8f74
erwinsantander <santandererwin4@gmail.com>
2024-12-10 01:40:20 -0800

    commit

--- delete_user.php
+++ delete_user.php
@@ -2,6 +2,13 @@
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
    page_require_level(1);
+
+   $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 ?>
 <?php
   $delete_id = delete_by_id('users',(int)$_GET['id']);
