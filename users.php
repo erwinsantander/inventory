12fb6664e05e7dcaa1dcfc164ba96b2ba82bec8d
erwinsantander <santandererwin4@gmail.com>
2024-12-10 01:56:58 -0800

    commit

--- users.php
+++ users.php
@@ -7,13 +7,6 @@
  page_require_level(1);
 //pull out all user form database
  $all_users = find_all_user();
-
- $request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}
 ?>
 <?php include_once('layouts/header.php'); ?>
 <div class="row">
