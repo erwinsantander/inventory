c8899bc7b45fcdd2c3310087d2ea1ba15b3b17f4
erwinsantander <santandererwin4@gmail.com>
2024-12-17 22:15:08 -0800

    commit

--- admin.php
+++ admin.php
@@ -197,7 +197,7 @@ if (substr($request, -4) == '.php') {
        <div class="panel-heading">
          <strong>
            <span class="glyphicon glyphicon-th"></span>
-           <span>Highest Selling Products</span>
+           <span>Highest Sellingaaaa Products</span>
          </strong>
        </div>
        <div class="panel-body">
