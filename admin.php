209847b39f25123fea57f87d99b94953d58538d5
erwinsantander <santandererwin4@gmail.com>
2024-12-17 22:46:10 -0800

    commit

--- admin.php
+++ admin.php
@@ -190,7 +190,7 @@ if (substr($request, -4) == '.php') {
 </script>
 
 <!-- Highest Selling Products Pie Chart -->
-<div class="row" style="width: 2200px; margin-left: 250px; margin-top: 24px; margin-right: 10px;">
+<div class="row" style="margin-left: 250px; margin-top: 24px; margin-right: 10px;">
   <div class="col-md-6">
     <div class="panel panel-default">
       <div class="panel-heading">
